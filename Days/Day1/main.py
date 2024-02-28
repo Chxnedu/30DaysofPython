d9a732428580544b9766ea91de76c2840e25d31d
Chxn-at-812627843742 <ojichinedu4@gmail.com>
2024-02-28 18:30:12 +0100

    Day 1

---
+++ Days/Day1/main.py
@@ -0,0 +1,8 @@
+# Day 1 is for string mainpulation and 
+print("hello world again")
+print("starting on the 28th of February")
+print('lets have "fun" for the next 30 days')
+print("testing new line \nanother line \nlast line")
+print("Hello, " + "Master " + input("what is your name?"))
+age = input("How old are you?")
+print("You are " + age + " years old")
---
+++ Days/Day1/project.py
@@ -0,0 +1,6 @@
+# Bandname Generator Project
+
+print("Hello, we're going to generate a potential band name for you!")
+city = input("What's the name of the city you grew up in?\n")
+pet =  input("What's the name of your pet?\n")
+print("Your band name could be " + city + " " + pet)
