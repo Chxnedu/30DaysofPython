01b80222be6d42558bb82fc1b25b710a09250021
Chxn-at-812627843742 <ojichinedu4@gmail.com>
2024-03-06 18:44:03 +0100

    Day 5 complete

--- Days/Day5/main.py
+++ Days/Day5/main.py
@@ -8,15 +8,50 @@ students_heights = input("Input a list of student heights ").split()
 for n in range(0, len(students_heights)):
     students_heights[n] = int(students_heights[n])
 print(students_heights)
+
 sum = 0
 for height in students_heights:
     sum += height
-print(sum)
 
 no_of_students = 0
 for height in students_heights:
     no_of_students += 1
-print(no_of_students)
 
 average = sum / no_of_students
 print(f"The average height of the students is {average}")
+
+
+# Code Challenge 2: Highest Score 
+student_scores = input("Input a list of student scores ").split()
+for n in range(0, len(student_scores)):
+    student_scores[n] = int(student_scores[n])
+print(student_scores)
+
+high_score = 0
+for score in student_scores:
+    if score > high_score:
+        high_score = score
+print(f"The highest score in the class is {high_score}")
+
+
+# Code Challenge 3: Evens Calculator
+total = 0
+for number in range(2, 101, +2):
+    total += number
+print(total)
+
+
+# Code Challenge 4: FizzBuzz
+for number in range(1, 101):
+# You could remove this first line and use else at the end
+    if number % 3 != 0 and number % 5 != 0:
+        print(number)
+    elif number % 3 == 0 and number % 5 == 0:
+        print("FizzBuzz")
+    elif number % 3 == 0:
+        print("Fizz")
+    elif number % 5 == 0:
+        print("Buzz")
+
+
+    
---
+++ Days/Day5/project.py
@@ -0,0 +1,22 @@
+#Password Generator Project
+import random
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters= int(input("How many letters would you like in your password?\n")) 
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+pass_letters = random.choices(letters, k = nr_letters)
+pass_symbols = random.choices(symbols, k = nr_symbols)
+pass_numbers = random.choices(numbers, k = nr_numbers)
+
+full_pass_list = pass_letters + pass_numbers + pass_symbols
+random.shuffle(full_pass_list)
+complete_pass = ""
+for n in range(0, len(full_pass_list)):
+    complete_pass += full_pass_list[n]
+print(f"Your password is {complete_pass}")
